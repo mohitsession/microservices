3e719e05dd0f9fae20c0de55a9252abb226bc608
admin <repo@test.com>
2025-11-15 18:42:35 -0500

    accounts microservice

---
+++ accounts/src/main/java/com/learn/accounts/AccountsApplication.java
@@ -0,0 +1,38 @@
+package com.learn.accounts;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.learn.accounts.controller") })
+@EnableJpaRepositories("com.learn.accounts.repository")
+@EntityScan("com.learn.accounts.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Ram",
+						email = "test@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.test.com"
+				)
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
